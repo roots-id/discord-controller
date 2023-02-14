b9ec210d077d2153d18428ef4cfcf2d9783e7ac4
Essbante <esteban.garcia@gmail.com>
2023-02-13 19:06:47 -0600

    Update README

--- src/main/kotlin/utils/Constants.kt
+++ src/main/kotlin/utils/Constants.kt
@@ -2,12 +2,6 @@ package utils
 
 object Constants {
     const val ISSUER_URL_DEFAULT = "http://localhost:8080/prism-agent"
-    const val HOLDER_URL_DEFAULT = "http://localhost:8090/prism-agent"
-    const val VERIFIER_URL_DEFAULT = "http://localhost:9000/prism-agent"
     const val ISSUER_URL = "ISSUER_URL"
     const val ISSUER_APIKEY = "ISSUER_APIKEY"
-    const val HOLDER_URL = "HOLDER_URL"
-    const val HOLDER_APIKEY = "HOLDER_APIKEY"
-    const val VERIFIER_URL = "VERIFIER_URL"
-    const val VERIFIER_APIKEY = "VERIFIER_APIKEY"
 }
